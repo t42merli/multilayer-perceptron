e31d9b4fe9bf6f089c9c7b7de443b9d45d6219c2
tmerli <theo.merli60@gmail.com>
2020-02-05 19:06:17 +0100

    e

--- graph.py
+++ graph.py
@@ -1,12 +1,21 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv('data.csv', index_col="id")
 
-gb = data.groupby('diagnosis')
+print (data.corr())
 
-for column in data.columns:
-	fig = plt.figure()
-	fig.suptitle(column)
-	if(data[column].dtype == 'int' or data[column].dtype == 'float'):
-		plt.hist(, bins=8, color=['red', 'blue'],label=[gb.groups], histtype='barstacked', alpha=0.5)
+# gb = data.groupby('diagnosis')
+# groups = [gb.get_group('M'), gb.get_group('B')]
+
+# for column in data.columns:
+# 	if((data[column].dtype == 'int' or data[column].dtype == 'float')):
+# 		fig = plt.figure()
+# 		fig.suptitle(column)
+# 		plt.hist([groups[0][column], groups[1][column]], bins=8, color=[
+#                  'red', 'blue'], label=['M', 'B'], histtype='barstacked', alpha=0.5)
+
+# plt.legend()
+# plt.show()
+
+# "extVal fractal dimension, extval smothness, stde fratal dim, stde concativity, stdE smoothness, stde texture, mean fractal dimennsion"
